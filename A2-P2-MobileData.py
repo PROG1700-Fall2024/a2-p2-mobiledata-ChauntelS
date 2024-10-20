a7382c7bbedfbac932a2f1bc5963fca523d0ab35
Your Name <you@example.com>
2024-10-20 18:35:47 -0300

    Programe 2 ready for marking

--- A2-P2-MobileData.py
+++ A2-P2-MobileData.py
@@ -5,12 +5,26 @@
 # Note, it is not a cumulative charge; your program will figure out which single range the usage falls into, 
 # then calculate the cost based on that range’s cost. 
 
-#Student #:     
-#Student Name:  
+#Student #: W0239497    
+#Student Name:  Chauntel Smith
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
-
+    #Variable
+    usageRate = 0
+    #Welcome message & User Input Variable
+    usage = float(input("Welcome, let us calculate your Usage Rate!\nEnter data usage in MB: "))
+    #Usage calculateions
+    if usage > 200 and (usage <= 500):
+        usageRate = usage * 0.105
+    elif usage > 500 and (usage <= 1000):
+        usageRate = usage * 0.110
+    elif usage > 1000:
+        usageRate = 118
+    else:
+        usageRate = 20
+    #Outcome message
+    print("Your Total charge is: ${0:.2f}".format(usageRate))
 
 
 
